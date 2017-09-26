709f88e070227f6ffda3d61d0e7030a3fa1aa6db
Matthew Grant <amattwgrant@gmail.com>
2017-09-25 22:17:01 -0400

    working on modal

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,29 @@
 //   message.style.display = 'block';
 // }
 
+
+// // Get the modal
+// var modal = document.getElementById("formPop");
+
+// // Get the button that opens it
+// var submit = document.getElementById("submit");
+
+// var span = document.getElementsByClassName("close")[0];
+
+
+// // Display the modal on click of submit button
+// submit.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // Close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // Close window clicking anywhere outside of it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }

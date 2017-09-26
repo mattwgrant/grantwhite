434af26d5bff4d37e665aecb1286e38040c900c9
Matthew Grant <amattwgrant@gmail.com>
2017-09-25 21:18:47 -0400

    dollar

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// var theSubmitButton = document.getElementById('submit');
+ 
+// theSubmitButton.onclick = function() {
+//   var theFormItself = document.getElementById('form');
+//   theFormItself.style.display = 'none'; 
+//   var theSuccessMessage = document.getElementById('message');
+//   message.style.display = 'block';
+// }
+

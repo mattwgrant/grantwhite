2592a426a7f6e555231715ec58e0ab5df0e03a38
Matthew Grant <matthewgrant@Matthews-MacBook-Pro.local>
2018-02-02 21:29:37 -0500

    dollar

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,8 @@
-// var theSubmitButton = document.getElementById('submit');
- 
-// theSubmitButton.onclick = function() {
-//   var theFormItself = document.getElementById('form');
-//   theFormItself.style.display = 'none'; 
-//   var theSuccessMessage = document.getElementById('message');
-//   message.style.display = 'block';
-// }
+var firstname = document.getElementById("firstname");
+// var lastname = document.getElementById("lastname");
 
-
-// // Get the modal
-// var modal = document.getElementById("formPop");
-
-// // Get the button that opens it
-// var submit = document.getElementById("submit");
-
-// var span = document.getElementsByClassName("close")[0];
-
-
-// // Display the modal on click of submit button
-// submit.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// // Close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // Close window clicking anywhere outside of it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
+firstname.addEventListener("input", function(event) {
+	if (firstname == "Matt") {
+		alert("Pleae enter your first name")
+	}
+});
